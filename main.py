0fc006e10df2ff2e1dc4ad09344e709f7ba94f2f
4eyes4u <kosta.grujcic@hotmail.com>
2022-08-16 02:07:20 +0200

    Refactoring: array of params to variadic.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import Union
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import linalg
-from scipy.optimize import least_squares, curve_fit
+from scipy.optimize import curve_fit
 from sklearn.model_selection import ParameterGrid
 
 CallableArg = Union[float, np.ndarray]
@@ -20,91 +20,76 @@ def configure_logging():
     logging.Formatter.converter = time.gmtime
 
 
-# def func(x: CallableArg, beta: np.ndarray):
-#     return np.exp(beta[0] * x + beta[1])
+def func_easy(x: CallableArg, a: float, b: float):
+    return np.exp(a * x + b)
 
 
-# def func(x: CallableArg, beta: np.ndarray):
-#     return beta[0] * x + beta[1]
-
-
-def func(x: CallableArg, beta: np.ndarray):
-    return beta[0] * np.cos(beta[1] * x) + beta[1] * np.sin(beta[0] * x)
-
-def func_(x, a, b):
+def func_hard(x: CallableArg, a: float, b: float):
     return a * np.cos(b * x) + b * np.sin(a * x)
 
 
-# def func(x: CallableArg, beta: np.ndarray):
-#     return beta[0] * x ** 2 + beta[1]
-
-
-def calc_jacobian(x: np.ndarray, beta: np.ndarray, delta: float = 1e-8):
+def calc_jacobian(func, x: np.ndarray, params: np.ndarray, delta: float = 1e-8):
     m = x.shape[0]
-    n = beta.shape[0]
+    n = params.shape[0]
 
     J = np.zeros((m, n), dtype=np.float32)
     for i in range(m):
         for j in range(n):
-            delta_j = np.zeros_like(beta)
+            delta_j = np.zeros_like(params)
             delta_j[j] = delta
+            delta_params = params + delta_j
 
-            diff = func(x[i], beta + delta_j) - func(x[i], beta)
+            diff = func(x[i], *delta_params) - func(x[i], *params)
             J[i, j] = diff / delta
 
     return J
 
 
-def calc_error(x: np.ndarray, y: np.ndarray, beta: np.ndarray):
-    r = y - func(x, beta)
+def calc_error(func, x: np.ndarray, y: np.ndarray, params: np.ndarray):
+    r = y - func(x, *params)
     error = np.sum(np.power(r, 2))
 
     return error
 
 
-# def solve_linear_system(A: np.ndarray, b: np.ndarray, x: np.ndarray):
-#     L = linalg.cholesky(A, lower=True)
+def run_single_iteration(func, x: np.ndarray, y: np.ndarray, params: np.ndarray, lmbd: float):
+    m = params.shape[0]
 
-
-def run_single_iteration(x: np.ndarray, y: np.ndarray, beta: np.ndarray, lmbd: float):
-    m = init_beta.shape[0]
-
-    J = calc_jacobian(x, beta)
+    J = calc_jacobian(func, x, params)
     A = J.T @ J + lmbd * np.eye(m)
-    r = y - func(x, beta)
+    r = y - func(x, *params)
     b = J.T @ r
 
-    beta_delta = linalg.solve(A, b)
-    beta_upd = beta + beta_delta
-    curr_error = calc_error(x, y, beta_upd)
+    params_delta = linalg.solve(A, b)
+    params_upd = params + params_delta
+    curr_error = calc_error(func, x, y, params_upd)
 
-    return beta_upd, curr_error
+    return params_upd, curr_error
 
 
-def levenberg_marquardt(x: np.ndarray, y: np.ndarray, init_beta: np.ndarray, init_lmbd: float,
+def levenberg_marquardt(func, x: np.ndarray, y: np.ndarray, init_params: np.ndarray, init_lmbd: float,
                         num_iters: int = 100, velocity: float = 1.1, error_tol: float = 1e-5, lmbd_tol: float = 5):
-    beta = np.copy(init_beta)
+    params = np.copy(init_params)
     lmbd = init_lmbd
 
     num_iters_len = len(str(num_iters)) - 1
     for curr_iter in range(num_iters):
-        beta, curr_error = run_single_iteration(x, y, beta, lmbd)
-
+        params, curr_error = run_single_iteration(func, x, y, params, lmbd)
         logging.debug(f"Current iteration {curr_iter:>{num_iters_len}}: error={curr_error:.5f} lmbd={lmbd:.5f}")
 
         if curr_error < error_tol:
             logging.warning("Error is lower than the threshold. Early stopping engaged.")
-            return beta
+            return params
 
-        _, error_same_lmbd = run_single_iteration(x, y, beta, lmbd)
+        _, error_same_lmbd = run_single_iteration(func, x, y, params, lmbd)
         smaller_lmbd = lmbd / velocity
-        _, error_smaller_lmbd = run_single_iteration(x, y, beta, smaller_lmbd)
+        _, error_smaller_lmbd = run_single_iteration(func, x, y, params, smaller_lmbd)
         if error_same_lmbd > curr_error and error_smaller_lmbd > curr_error:
             larger_lmbd = lmbd * velocity
-            _, error_larger_lmbd = run_single_iteration(x, y, beta, larger_lmbd)
+            _, error_larger_lmbd = run_single_iteration(func, x, y, params, larger_lmbd)
             while error_larger_lmbd > curr_error and larger_lmbd < lmbd_tol:
                 larger_lmbd = larger_lmbd * velocity
-                _, error_larger_lmbd = run_single_iteration(x, y, beta, larger_lmbd)
+                _, error_larger_lmbd = run_single_iteration(func, x, y, params, larger_lmbd)
 
             lmbd = min(larger_lmbd, lmbd_tol)
         elif error_same_lmbd < curr_error and error_smaller_lmbd > curr_error:
@@ -116,7 +101,7 @@ def levenberg_marquardt(x: np.ndarray, y: np.ndarray, init_beta: np.ndarray, ini
         else:
             lmbd = smaller_lmbd
 
-    return beta
+    return params
 
 
 if __name__ == "__main__":
@@ -138,26 +123,30 @@ if __name__ == "__main__":
     # x = np.linspace(0, 10, n)
 
     y = target(x)
-    init_beta = np.array([100, 100], dtype=np.float32)
-    beta = curve_fit(func_, x, y, init_beta)[0]
-    logging.info(f"curve_fit: {beta}")
+    plt.plot(x, y, color="blue")
 
-    param_grid = ParameterGrid({"init_lmbd": np.linspace(1e-3, 1e-1, 10),
-                                "velocity": np.linspace(1.1, 2, 5)})
+    init_params = np.array([100, 100], dtype=np.float32)
+
+    params_scipy = curve_fit(func_hard, x, y, init_params)[0]
+    logging.info(f"scipy: {params_scipy}")
+    plt.plot(x, func_hard(x, *params_scipy), color="green")
+
+    hyperparam_grid = ParameterGrid({"init_lmbd": np.linspace(1e-3, 1e-1, 10),
+                                     "velocity": np.linspace(1.1, 2, 5)})
 
     results = []
-    for params in param_grid:
-        beta = levenberg_marquardt(x, y, init_beta, **params)
-        curr_error = calc_error(x, y, beta)
-        results.append((curr_error, params, beta))
+    for hyperparams in hyperparam_grid:
+        logging.info(f"hyperparams: {hyperparams}")
+        params = levenberg_marquardt(func_hard, x, y, init_params, **hyperparams)
+        curr_error = calc_error(func_hard, x, y, params)
+        results.append((curr_error, hyperparams, params))
 
     results = sorted(results, key=lambda x: x[0])
     errors = list(map(lambda r: r[0], results))
+    logging.info(f"optimal hyperparams: {results[0][1]}")
 
-    beta_lm = results[0][2]
-    logging.info(f"Solution: {beta_lm}")
+    params_lm = results[0][2]
+    logging.info(f"levenberg_marquardt: {params_lm}")
+    plt.plot(x, func_hard(x, *params_lm), color="red")
 
-    plt.plot(x, y, color="blue")
-    plt.plot(x, func_(x, *beta), color="green")
-    plt.plot(x, func(x, beta_lm), color="red")
     plt.show()
